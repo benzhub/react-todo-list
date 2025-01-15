16fb533b5ceec21b05769a1a39823e72c491ff57
Len Rich <lenrich1403@gmail.com>
2025-01-15 18:07:02 +0800

    feat: Header Component props

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="p-4 flex justify-center items-center h-screen">
       <main className="pt-10 pb-4 border border-gray-200/30 w-[500px] grid grid-cols-1 grid-rows-[1fr_auto] rounded-lg bg-gradient-to-t from-[#f2ebf8] to-[#e5f8f9]">
         <div className="flex flex-col">
-          <Header />
+          <Header title="Todo List" description="Add things to do" />
           <div className="h-[2px] bg-[#c7c5c5] mx-4" />
           <Progress percentage={50}/>
           <TaskList />
