af39e575bc4a01a309602935da0c2b5cfa23d1af
Len Rich <lenrich1403@gmail.com>
2025-01-15 22:48:57 +0800

    feat: setup react-router-dom

---
+++ src/pages/Home.js
@@ -0,0 +1,76 @@
+import Header from "../components/Header";
+import TaskList from "../components/TaskList";
+import Progress from "../components/Progress";
+import Switch from "../components/Switch";
+import AddTask from "../components/AddTask";
+import { useState } from "react";
+import { useAtom } from "jotai";
+import { taskListAtom } from "../store/TaskListAtom";
+import { useSearchParams } from "react-router-dom";
+
+const Home = () => {
+  const [taskList, setTaskList] = useAtom(taskListAtom);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [isSortedByCompleted, setIsSortedByCompleted] = useState(
+    searchParams.get("sort") === "true"
+  );
+
+  const handleAddTask = (task) => {
+    setTaskList((prev) => [...prev, task]);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTaskList((prev) => prev.filter((task) => task.id !== taskId));
+  };
+
+  const handleUpdateTaskStatus = (taskId) => {
+    setTaskList((prev) =>
+      prev.map((task) =>
+        task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task
+      )
+    );
+  };
+
+  const handleIsCheckedSwitchToggle = () => {
+    setIsSortedByCompleted((prev) => !prev);
+    setSearchParams({ sort: (!isSortedByCompleted).toString() });
+  };
+
+  const percentage = Math.floor(
+    taskList.length === 0
+      ? 0
+      : (taskList.filter((task) => task.isCompleted).length / taskList.length) *
+          100
+  );
+
+  const sortedTaskList = isSortedByCompleted
+    ? [
+        ...taskList.filter((task) => !task.isCompleted),
+        ...taskList.filter((task) => task.isCompleted),
+      ]
+    : taskList;
+  return (
+    <div className="flex h-[100dvh] sm:p-4 sm:justify-center sm:items-center">
+      <main className="border border-gray-200/30 w-[500px] grid grid-cols-1  rounded-lg bg-gradient-to-t from-[#efe3f4] to-[#eaf7f8]">
+        <div className="flex flex-col">
+          <Header title="Todo List" description="Add things to do" />
+          <div className="h-[2px] bg-[#c7c5c5] mx-4" />
+          <Progress percentage={percentage} />
+          <TaskList
+            taskList={sortedTaskList}
+            onDeleteTask={handleDeleteTask}
+            onUpdateTaskStatus={handleUpdateTaskStatus}
+          />
+          <div className="h-[2px] bg-[#c7c5c5] mx-4" />
+          <Switch
+            checked={isSortedByCompleted}
+            onChange={handleIsCheckedSwitchToggle}
+          />
+        </div>
+        <AddTask onAddTask={handleAddTask} />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
