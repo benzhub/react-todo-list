3781762099c515f2508540fd657eeeeb79370adb
Len Rich <lenrich1403@gmail.com>
2025-01-15 18:29:00 +0800

    style: AddTask Component Layout

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const AddTask = () => {
   return (
-    <div className='p-4'>AddTask</div>
+    <div className='p-4'>
+      <p className='text-gray-600/70'>Add to list</p>
+      <div className='flex gap-1'>
+        <input type="text" className='w-full p-2 rounded-md border border-gray-200/30' />
+        <button className='bg-indigo-500/80 py-2 px-4 rounded-md'>
+          <span className='text-white text-2xl font-bold'>&#43;</span>
+        </button>
+      </div>
+    </div>
   )
 }
 
