5477c9b7c76815d32eca656306647ccb213c4b07
Len Rich <lenrich1403@gmail.com>
2025-01-16 00:23:25 +0800

    fix: Progress Component style

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -8,7 +8,7 @@ function Progress ({ percentage = 0 }) {
   return (
     <div className="p-4 flex items-center gap-2">
       <span className="inline-block text-gray-600/70">{percentage}%</span>
-      <div className="bg-white rounded-full h-3 flex-1 mr-4">
+      <div className="bg-white rounded-full h-3 flex-1 sm:mr-4">
         <div
           className="h-3 rounded-full transition-color duration-300 ease-in-out bg-[#7e9fe5]"
           style={{
