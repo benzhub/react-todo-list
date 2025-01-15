34f094179ce3d513e1a3e548d012e126eec75bb1
Len Rich <lenrich1403@gmail.com>
2025-01-15 18:05:21 +0800

    style: Progress Component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import AddTask from "./components/AddTask";
 function App() {
   return (
     <div className="p-4 flex justify-center items-center h-screen">
-      <main className="pt-10 pb-4 border border-gray-200/30 w-[500px] grid grid-cols-1 grid-rows-[1fr_auto] rounded-lg bg-gradient-to-t from-[#f2ebf8] to-[#f1feff]">
+      <main className="pt-10 pb-4 border border-gray-200/30 w-[500px] grid grid-cols-1 grid-rows-[1fr_auto] rounded-lg bg-gradient-to-t from-[#f2ebf8] to-[#e5f8f9]">
         <div className="flex flex-col">
           <Header />
           <div className="h-[2px] bg-[#c7c5c5] mx-4" />
-          <Progress />
+          <Progress percentage={50}/>
           <TaskList />
           <div className="h-[2px] bg-[#c7c5c5] mx-4" />
           <Switch />
